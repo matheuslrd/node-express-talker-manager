52ae222215d980c117471aebd0b982a61b2d7dc0
Matheus Laurindo <matheus104521@gmail.com>
2022-01-14 15:24:20 -0300

    Conclui requisito 1

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
+const fileSystem = require('fs');
+
 const app = express();
 app.use(bodyParser.json());
 
@@ -10,6 +12,12 @@ const PORT = '3000';
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
+});
+
+app.get('/talker', (_req, res) => {
+  const speakers = fileSystem.readFileSync('./talker.json', 'utf-8');
+
+  res.status(200).send(JSON.parse(speakers));
 });
 
 app.listen(PORT, () => {
